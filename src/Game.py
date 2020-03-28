929c1bea3eb2fe3c163d442761ede692ae88e37e
Jacob Heard <heard.jacob.7@gmail.com>
2020-03-28 12:00:59 -0400

    Moved load_game to Game.py; moved 'private' methods into Util

--- src/Game.py
+++ src/Game.py
@@ -7,8 +7,16 @@ class Game(object):
         self.rooms = []
         self.player = []
     
-    def load_game(self, game_path : str) -> None:
-        pass
-    
     def save_game_state(self, save_path : str) -> None:
         pass
+    
+    @staticmethod
+    def load_game(directory : str) -> Game:
+        """
+        Loads yaml files from input directory into a game object
+        """
+        enemies = Util.load_yaml_data( Util.get_yaml_filename("enemies") )
+        rooms = Util.load_yaml_data( Util.get_yaml_filename("rooms") )
+        items = Util.load_yaml_data( Util.get_yaml_filename("items") )
+        player = Util.load_yaml_data( Util.get_yaml_filename("player") )
+        return Game(enemies, items, rooms, player)
