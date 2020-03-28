34a2d4719543bb5e542bf75c66a79eda990d3297
Jacob Heard <heard.jacob.7@gmail.com>
2020-03-28 18:11:09 -0400

    added docstrings for all classes/functions; fixed some type hinting that was out of date

--- src/GameError.py
+++ src/GameError.py
@@ -1,2 +1,7 @@
 class GameError(Exception):
+    """
+    A custom exception for providing more information to the user.
+    The general use of this class is to catch an exception, add some
+    additional information, then raise GameError
+    """
     pass
